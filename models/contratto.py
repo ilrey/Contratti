e5de2e474e054d5bf13818584cbb62aea09d94f9
redon.copa <redon.copa@nttdata.com>
2023-01-03 15:36:10 +0100

    15.0

---
+++ models/contratto.py
@@ -0,0 +1,10 @@
+from odoo import api, fields, models
+
+class ContrattiContratto(models.Model):
+    _name = "contratti.contratto"
+    _description = "Contratto"
+
+    Con_CognomeClienteContratto = fields.Char(string='Cognome')
+    Con_NomeCliente = fields.Char(string='Nome')
+    Con_CodFiscaleCliente = fields.Char(string='Codice Fiscale')
+    Con_TipoDocumento = fields.Selection([('Carta_identità','Carta identità'), ('patente', 'Patente'), ('passaporto', 'Passaporto')])
