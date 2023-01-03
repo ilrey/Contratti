865654c495ee68c1a8381048c338d6cd7c17ac43
redon.copa <redon.copa@nttdata.com>
2023-01-03 18:17:53 +0100

    15.0

--- models/contratto.py
+++ models/contratto.py
@@ -5,11 +5,9 @@ from odoo import api, fields, models
 class ContrattiContratto(models.Model):
     _name = "contratti.contratto"
     _description = "Contratto"
-    _inherit = 'res.partner'
 
     Con_CognomeClienteContratto = fields.Char(string='Cognome')
     Con_NomeCliente = fields.Char(string='Nome')
     Con_CodFiscaleCliente = fields.Char(string='Codice Fiscale')
     active = fields.Boolean(string="Active", default=True)
     Con_TipoDocumento = fields.Selection([('Carta_identita','Carta identita'), ('patente', 'Patente'), ('passaporto', 'Passaporto')])
-    Con_PartnerId = fields.Many2One('res.partner', string="Agente")
