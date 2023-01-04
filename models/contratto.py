8fdcd48216ab909ff8850c2908aaeb6a35e25790
redon.copa <redon.copa@nttdata.com>
2023-01-04 17:07:26 +0100

    15.0

--- models/contratto.py
+++ models/contratto.py
@@ -33,16 +33,11 @@ class ContrattiContratto(models.Model):
 
 
     def open_file_upload(self):
-        valori = {
-            'name': 'prova',
-            'tag_ids': 'ss'
-            }
         return{
             'res_model': 'dms.file',
             #'res_id': 12,
             'type': 'ir.actions.act_window',
             'view_mode': 'form',
             'view_id': self.env.ref('dms.view_dms_file_form').id,
-            'context': {'name': self.id} 
             }
     
