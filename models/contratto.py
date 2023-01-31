356efc0d75b2d4bf15a74451101c1b644833ec53
Redon Rey Copa <coparei7@gmail.com>
2023-01-31 16:11:59 +0100

    15.0

--- models/contratto.py
+++ models/contratto.py
@@ -1,6 +1,5 @@
 from dataclasses import field
 from email.policy import default
-import string
 from odoo import api, fields, models
 from odoo.exceptions import ValidationError
 
@@ -102,10 +101,8 @@ class ContrattiContratto(models.Model):
                 vals[i] = vals[i].upper()
         return super(ContrattiContratto, self).write(vals)
 
-    def buttone_prova(slef):       
-        delta = self.env['dms.file'].search([])
-        delta = str(delta.mapped('name'))
-        raise ValidationError(_(delta))
+    def buttone_prova(self):       
+        delta = self.env['dms.file'].create({'name' : 'provafie'})
         return delta
 
 
