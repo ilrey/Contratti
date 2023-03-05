afad077a7f23cfc0114526cdc19d475c9d9c82a5
ilrey <coparei7@gmail.com>
2023-03-05 21:18:22 +0100

    15.0

--- models/contratto.py
+++ models/contratto.py
@@ -139,14 +139,16 @@ class ContrattiContratto(models.Model):
     @api.model
     def create(self, vals):
         for i in list(vals):
-            if type(vals[i]) != bool and type(vals[i]) != int and type(vals[i]) != list:
-                vals[i] = vals[i].upper()
+            if type(vals[i]) != bool and type(vals[i]) != int:
+                if type(vals[i]) != list:
+                    vals[i] = vals[i].upper()
         return super(ContrattiContratto, self).create(vals)
 
     def write(self, vals):
         for i in list(vals):
-            if type(vals[i]) != bool and type(vals[i]) != int and type(vals[i]) != list:
-                vals[i] = vals[i].upper()
+            if type(vals[i]) != bool and type(vals[i]) != int:
+                if type(vals[i]) != list:
+                    vals[i] = vals[i].upper()
         return super(ContrattiContratto, self).write(vals)
 
     # def buttone_prova(self):
